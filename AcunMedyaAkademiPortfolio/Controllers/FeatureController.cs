340aa9f82b79c3192f6e52e52b939b5b6fb649fd
Gökay KUTLUK <gokaykutluk1@gmail.com>
2024-12-31 15:46:01 +0300

    ADIM 2

--- AcunMedyaAkademiPortfolio/Controllers/FeatureController.cs
+++ AcunMedyaAkademiPortfolio/Controllers/FeatureController.cs
@@ -3,15 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AcunMedyaAkademiPortfolio.Models;
+
 
 namespace AcunMedyaAkademiPortfolio.Controllers
 {
     public class FeatureController : Controller
     {
+        DbportfolioEntities db = new DbportfolioEntities();
         // GET: Feature
         public ActionResult Index()
         {
+            var values = db.TblFeature.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult CreateFeature()
+        {
             return View();
+        }
+        [HttpPost]
+
+        public ActionResult CreateFeature(TblFeature p)
+        {
+            db.TblFeature.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteFeature(int id)
+        {
+            var value = db.TblFeature.Find(id);
+            db.TblFeature.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult UpdateFeature(int id)
+        {
+            var value = db.TblFeature.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateFeature(TblFeature p)
+        {
+            var value = db.TblFeature.Find(p.FeatureId);
+            value.FeatureTitle = p.FeatureTitle;
+            value.FeatureSubtitle = p.FeatureSubtitle;
+            value.FeatureImageUr1 = p.FeatureImageUr1;
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
